8e8264aa92b08dfb0a2b4e9513057cfad9a9660f
Muhammadyusuf <mmuhammadyusuf302@gmail.com>
2024-10-17 18:09:39 +0500

    loding

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { Header } from './components';
 import Main from './components/main';
 import Footer from './components/footer';
+import loadings from "./assets/loading.gif";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import ToggleModal from './components/toggleModal';
 import './i18n'; 
+
 AOS.init();
 
 export const StateContext = createContext({});
+
 const App = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [loading, setLoading] = useState(true); // Yuklash holati
+
+  useEffect(() => {
+    // 3 soniya o'tgach, yuklashni to'xtatamiz
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer); // Komponent o'chirilganda timer ni tozalaymiz
+  }, []);
 
   return (
     <StateContext.Provider value={{ modalOpen, setModalOpen }}>
       <div className="font-mono">
-        <Header />
-        <Main />
-        <Footer />
-        <ToggleModal />
+        {loading ? (
+          <div className="flex justify-center items-center h-screen">
+            <img src={loadings} alt="iflay" />
+          </div>
+        ) : (
+          <>
+            <Header />
+            <Main />
+            <Footer />
+            <ToggleModal />
+          </>
+        )}
       </div>
     </StateContext.Provider>
   );
 };
 
 export default App;
+
